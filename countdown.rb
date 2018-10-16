dd0aa3ef2d33d0728ce36aa61a75120d87f0f174
ktochow1 <ktochow1@binghamton.edu>
2018-10-16 17:42:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,17 +11,17 @@ end
   end
 end
 
-# def countdown_with_sleep(5)
-#   timer 
-#   while timer > 0 
-#   sleep(5)
-#   puts "#{timer} SECOND(S)!"
-#   timer -= 1
-# end
-# end
+def countdown_with_sleep(5)
+  timer 
+  while timer > 0 
+  sleep(5)
+  puts "#{timer} SECOND(S)!"
+  timer -= 1
+end
+end
 
-# x = 1
-# while x < 10
-#   puts "#{x} is less than 10"
-#   x += 1
-# end
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
+end
