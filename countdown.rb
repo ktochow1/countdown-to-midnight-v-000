42fbe0d23c8719452c9a1e09fb865b10df07be6b
ktochow1 <ktochow1@binghamton.edu>
2018-10-16 17:42:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(timer)
+  timer
+  while timer > 0  
+  puts "#{timer} SECOND(S)!"
+  timer -= 1
 end
+  if timer = 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(5)
+  timer 
+  while timer > 0 
+  sleep(5)
+  puts "#{timer} SECOND(S)!"
+  timer -= 1
+end
+end
+
+# x = 1
+# while x < 10
+#   puts "#{x} is less than 10"
+#   x += 1
+# end
